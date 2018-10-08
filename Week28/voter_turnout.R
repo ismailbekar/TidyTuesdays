e87c235bceb7a2d12f7c1868edb3f52262772740
Ismail Bekar <39299023+ismailbekar@users.noreply.github.com>
2018-10-09 01:14:05 +0200

    guncelleme

--- Week28/voter_turnout.R
+++ Week28/voter_turnout.R
@@ -5,18 +5,36 @@ library(tidyverse)
 vote <- read.csv("voter_turnout.csv")
 vote <- tbl_df(vote)
 
-summary(vote2$state)
+vote <- vote %>% 
+     mutate(percentage = votes/eligible_voters) %>% 
+     mutate(percentage = round(percentage, 2))
+
+table(vote$state)
 
 vote2 <- na.omit(vote)
 
 vote2 <- vote2 %>% 
           group_by(state) %>% 
-          filter(n() > 8)
+          filter(n() > 8) 
 
-summary(vote2$state)
+table(vote2$state)
 
 
-ggplot(vote2, aes(votes, eligible_voters, group= year)) +
-          geom_curve() +
+ggplot(vote2, aes(percentage, fill= year)) +
+          geom_histogram() +
           facet_wrap(~ state)
+vote3 <- reshape2::melt(vote2)
 
+vote2 <- vote2 %>% filter(year== 2014)
+
+ggplot(vote2, aes(x = percentage, y = year)) +
+     geom_density_ridges() +
+     scale_fill_viridis_c(option = "D") +
+     theme_ridges() +
+     theme(plot.caption = element_text(colour = "grey50")) +
+     labs(x = "Births per day",
+          y = "",
+          title = "Births per Weekday",
+          subtitle = "Measured in the US | 2000-2014",
+          fill = "Births \n per day",
+          caption = "Data: ssa.gov | Source: Fivethirtyeight | Plot by @othomn")
