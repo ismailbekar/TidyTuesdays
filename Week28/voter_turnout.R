b1d7b7a8ed3be1d305c2a34682f1b4d333a38e7c
Ismail Bekar <ismail.bekar@usys.ethz.ch>
2018-10-08 17:21:49 +0200

    guncelleme

--- Week28/voter_turnout.R
+++ Week28/voter_turnout.R
@@ -1,3 +1,22 @@
 setwd("~/GitHub/R4DS/TidyTuesdays/Week28")
 
+library(tidyverse)
+
 vote <- read.csv("voter_turnout.csv")
+vote <- tbl_df(vote)
+
+summary(vote2$state)
+
+vote2 <- na.omit(vote)
+
+vote2 <- vote2 %>% 
+          group_by(state) %>% 
+          filter(n() > 8)
+
+summary(vote2$state)
+
+
+ggplot(vote2, aes(votes, eligible_voters, group= year)) +
+          geom_curve() +
+          facet_wrap(~ state)
+
