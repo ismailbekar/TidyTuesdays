4fdf3f401cc0e1358e084371ec0cb205e7a47a8f
Ismail Bekar <ismail.bekar@usys.ethz.ch>
2018-10-10 13:16:12 +0200

    Week 28

--- Week28/voter_turnout.R
+++ Week28/voter_turnout.R
@@ -1,40 +1,44 @@
 setwd("~/GitHub/R4DS/TidyTuesdays/Week28")
 
 library(tidyverse)
+library(ggridges)
 
 vote <- read.csv("voter_turnout.csv")
 vote <- tbl_df(vote)
 
-vote <- vote %>% 
-     mutate(percentage = votes/eligible_voters) %>% 
-     mutate(percentage = round(percentage, 2))
+vote <- vote %>%
+          mutate(percentage = (votes/eligible_voters)*100) %>%
+          mutate(percentage = round(percentage, 2)) %>% 
+          mutate(type= case_when(year %in% c(1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012) ~ "presidental",
+                                  year %in% c(1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014) ~ "midterm" )) %>% 
+          select(id, year, type, state, eligible_voters, votes, percentage)
 
-table(vote$state)
-
+summary(vote$state)
 vote2 <- na.omit(vote)
 
 vote2 <- vote2 %>% 
           group_by(state) %>% 
-          filter(n() > 8) 
+          filter(n() > 9)
 
-table(vote2$state)
+summary(vote2$state)
 
+vote_means <- vote2 %>% 
+          group_by(state) %>% 
+          summarise(avg= round(mean(percentage), 2)) %>% 
+          arrange(avg)
 
-ggplot(vote2, aes(percentage, fill= year)) +
-          geom_histogram() +
-          facet_wrap(~ state)
-vote3 <- reshape2::melt(vote2)
+vote2 <- left_join(vote2, vote_means, by="state") %>% 
+          arrange(avg)
 
-vote2 <- vote2 %>% filter(year== 2014)
-
-ggplot(vote2, aes(x = percentage, y = year)) +
-     geom_density_ridges() +
-     scale_fill_viridis_c(option = "D") +
-     theme_ridges() +
-     theme(plot.caption = element_text(colour = "grey50")) +
-     labs(x = "Births per day",
-          y = "",
-          title = "Births per Weekday",
-          subtitle = "Measured in the US | 2000-2014",
-          fill = "Births \n per day",
-          caption = "Data: ssa.gov | Source: Fivethirtyeight | Plot by @othomn")
+ggplot(vote2, aes(x = percentage, y = reorder(state, avg),  fill = type)) +
+          geom_density_ridges(scale = 2, size = 0.15, rel_min_height = 0.001, alpha = .8, 
+                              jittered_points = TRUE, point_shape = "|", point_size = 2) +
+          scale_x_continuous(limits = c(25, 85)) +
+          labs(x = "Vote turnout (%)",
+               y = "State",
+               fill= "Election Type",
+               title = "US Voter Turnout (1980-2014)",
+               subtitle = "Midterm vs. Presidental",
+               caption = "States that have data lower than half of the elections are removed
+                          Data Source: data.world | Plot by @fire_ecologist" ) +
+          theme_light()
