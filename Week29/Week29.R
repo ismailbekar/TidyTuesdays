432263f58bad36c0d4b9fcda75d8c1bdb6649332
Ismail Bekar <39299023+ismailbekar@users.noreply.github.com>
2018-10-21 00:32:22 +0200

    week 29

--- Week29/Week29.R
+++ Week29/Week29.R
@@ -1,16 +1,32 @@
 setwd("~/GitHub/R4DS/TidyTuesdays/Week29")
 library(tidyverse)
+library(ggplot2)
 
 college <- as.tibble(read.csv("CollegeMajor.csv"))
 
-ggplot(college, aes(ShareWomen, Median, group= Major_category)) +
-     geom_point()
-
+college <- college %>% 
+     filter(Major_category != "Interdisciplinary")
+     
 ggplot(college, aes(reorder(Major_category, Unemployment_rate, FUN= mean), 
                     Unemployment_rate, 
                     fill= Major_category)) +
      geom_boxplot() +
-     labs( fill= "Category") +
+     labs(title = "Unemployment rate by major categories",
+           subtitle= "Colors are not ordered",
+           x = "Major Category",
+           y = "Unemployment",
+          fill= "Category") +
+     coord_flip()
+
+ggplot(college, aes(reorder(Major_category, Unemployment_rate, FUN= mean), 
+                    Unemployment_rate, 
+                    fill= reorder(Major_category, Unemployment_rate))) +
+     geom_boxplot() +
+     labs(title = "Unemployment rate by major categories",
+          subtitle= "Colors are ordered",
+          x = "Major Category",
+          y = "Unemployment",
+          fill= "Category") +
      coord_flip()
 
 ggplot(college, aes(reorder(Major_category, Unemployment_rate, FUN= median), 
@@ -18,3 +34,6 @@ ggplot(college, aes(reorder(Major_category, Unemployment_rate, FUN= median),
      geom_boxplot() +
      coord_flip() +
      theme_light()
+
+ggplot(college, aes(ShareWomen, Median, group= Major_category)) +
+     geom_point()
